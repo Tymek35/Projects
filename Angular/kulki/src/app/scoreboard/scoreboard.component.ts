008772a14ac25f263f0f1fd2cc867e8e0cdbd355
Tymek35 <tymoteusz.spychaj35@gmail.com>
2022-10-27 00:30:44 +0200

    Dummy scoreboard added and layout fix

--- Angular/kulki/src/app/scoreboard/scoreboard.component.ts
+++ Angular/kulki/src/app/scoreboard/scoreboard.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
-  selector: 'app-scoreboard',
-  templateUrl: './scoreboard.component.html',
-  styleUrls: ['./scoreboard.component.scss']
+    selector: 'app-scoreboard',
+    templateUrl: './scoreboard.component.html',
+    styleUrls: ['./scoreboard.component.scss']
 })
 export class ScoreboardComponent implements OnInit {
+    elem: any;
+    fullscreen_on: boolean = false;
+    next: string[] = ['blue', 'green', 'yellow']
 
-  constructor() { }
+    constructor(@Inject(DOCUMENT) private document: any) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.elem = document.documentElement;
+    }
+
+    fullscreen(): void {
+        if(!this.fullscreen_on) {
+            if (this.elem.requestFullscreen) {
+                this.elem.requestFullscreen();
+            } else if (this.elem.mozRequestFullScreen) {
+                /* Firefox */
+                this.elem.mozRequestFullScreen();
+            } else if (this.elem.webkitRequestFullscreen) {
+                /* Chrome, Safari and Opera */
+                this.elem.webkitRequestFullscreen();
+            } else if (this.elem.msRequestFullscreen) {
+                /* IE/Edge */
+                this.elem.msRequestFullscreen();
+            }
+            this.fullscreen_on = true;
+        } else {
+            if (this.document.exitFullscreen) {
+                this.document.exitFullscreen();
+            } else if (this.document.mozCancelFullScreen) {
+                /* Firefox */
+                this.document.mozCancelFullScreen();
+            } else if (this.document.webkitExitFullscreen) {
+                /* Chrome, Safari and Opera */
+                this.document.webkitExitFullscreen();
+            } else if (this.document.msExitFullscreen) {
+                /* IE/Edge */
+                this.document.msExitFullscreen();
+            }
+            this.fullscreen_on = false;
+        }
+    }
 
 }
