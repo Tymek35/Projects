006121214930157222ffc33c26b65faba19162f3
Tymoteusz Spychaj <tymoteusz.spychaj35@gmail.com>
2024-04-27 22:22:29 +0200

    Refreshed migrations and added cors headers

--- Python/kulki_api/kulki_api/api/models.py
+++ Python/kulki_api/kulki_api/api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Score(models.Model):
-    playerName = models.CharField(max_length=20)
+    playerName = models.CharField(max_length=14)
     score = models.IntegerField()
     scoreDate = models.DateTimeField("score date", auto_now_add=True)
--- Python/kulki_api/kulki_api/api/views.py
+++ Python/kulki_api/kulki_api/api/views.py
@@ -3,6 +3,10 @@ from django.views.generic import View
 from api.models import Score
 import json
 
+def addAccessControlAllowOriginHeader(response):
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
+
 
 class SaveScoreView(View):
     def put(self, request):
@@ -11,22 +15,27 @@ class SaveScoreView(View):
             playerName = body["playerName"]
             score = body["score"]
         except:
-            return HttpResponse(status=400)
+            return addAccessControlAllowOriginHeader(HttpResponse(status=400))
 
         if not isinstance(playerName, str) or not isinstance(score, int):
-            return HttpResponse(status=400)
+            return addAccessControlAllowOriginHeader(HttpResponse(status=400))
         
-        if len(playerName) > 20 or len(playerName) < 1:
-            return HttpResponse(status=400)
+        if len(playerName) > 14 or len(playerName) < 1:
+            return addAccessControlAllowOriginHeader(HttpResponse(status=400))
         
         scoreObj = Score(playerName=playerName, score=score)
         scoreObj.save()
-        return HttpResponse(status=201)
+        return addAccessControlAllowOriginHeader(HttpResponse(status=201))
+    
+    def options(self, request):
+        response = super(SaveScoreView, self).options(request)
+        response["Access-Control-Allow-Methods"] = "PUT, OPTIONS"
+        response["Access-Control-Allow-Headers"] = "*"
+        return addAccessControlAllowOriginHeader(response)
 
 
 class GetScoresView(View):
     def get(self, request): 
         scores = Score.objects.all().order_by("-scoreDate").values()
         response = JsonResponse(list(scores), safe=False)
-        response["Access-Control-Allow-Origin"] = "*"
-        return response
+        return addAccessControlAllowOriginHeader(response)
