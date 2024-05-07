f02e898f1b486aa3ad799f8e908e28167856b07c
Tymoteusz Spychaj <tymoteusz.spychaj35@gmail.com>
2024-05-07 19:29:10 +0200

    Fixes in production configuration

--- Python/kulki_api/kulki_api/api/views.py
+++ Python/kulki_api/kulki_api/api/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from api.models import Score
 import json
 
+
 def addAccessControlAllowOriginHeader(response):
     response["Access-Control-Allow-Origin"] = "*"
     return response
@@ -14,19 +15,19 @@ class SaveScoreView(View):
             body = json.loads(request.body)
             playerName = body["playerName"]
             score = body["score"]
-        except:
+        except Exception:
             return addAccessControlAllowOriginHeader(HttpResponse(status=400))
 
         if not isinstance(playerName, str) or not isinstance(score, int):
             return addAccessControlAllowOriginHeader(HttpResponse(status=400))
-        
+
         if len(playerName) > 14 or len(playerName) < 1:
             return addAccessControlAllowOriginHeader(HttpResponse(status=400))
-        
+
         scoreObj = Score(playerName=playerName, score=score)
         scoreObj.save()
         return addAccessControlAllowOriginHeader(HttpResponse(status=201))
-    
+
     def options(self, request):
         response = super(SaveScoreView, self).options(request)
         response["Access-Control-Allow-Methods"] = "PUT, OPTIONS"
@@ -35,7 +36,7 @@ class SaveScoreView(View):
 
 
 class GetScoresView(View):
-    def get(self, request): 
+    def get(self, request):
         scores = Score.objects.all().order_by("-scoreDate").values()
         response = JsonResponse(list(scores), safe=False)
         return addAccessControlAllowOriginHeader(response)
